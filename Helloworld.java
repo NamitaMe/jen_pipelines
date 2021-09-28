6412749c1f91cca79b408107d74d15bcaaae81e5
NamitaMe <88753484+NamitaMe@users.noreply.github.com>
2021-09-27 18:41:39 -0700

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
-  String name = "Nami";
+  static String name = "Nami";
   
   public static void main(String[] args){
-    System.out.println("Hello: " + Helloworld().name);
+    System.out.println("Hello: " + name);
   }
 }
